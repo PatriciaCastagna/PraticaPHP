deb1c67d733b2ffb69d34e202ae816bdd7cca33f
Tissia <tissiapsicology@gmail.com>
2022-01-05 14:55:31 +0100

    Integração da primeira parte do desafio

---
+++ Desafio/desafio.php
@@ -0,0 +1,27 @@
+<?php  
+$products = [
+    'preto' =>  [
+        'PP' => 1,
+        'M' => 1,
+        'G' => 1,
+        'GG' => 2
+    ],
+    'branco' => [
+        'PP'=> 1,
+        'G' => 1
+    ],
+    'vermelho' => [
+        'M' => 1
+    ],
+    'azul' => [
+        'XG' => 3,
+        'P' => 1
+    ]
+];
+
+   print_r($products['preto']);
+    
+
+
+
+?>
